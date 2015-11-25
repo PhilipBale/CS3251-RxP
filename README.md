e4457b1e0e3578746cca3252d6d11519bc4b2bd5
theoryoflol <theoryoflol@gmail.com>
2015-11-25 15:53:53 -0500

    Rearranged code to make it cleaner with main and grouped up helper functions in Fxa Client and Fxa Server. Also added in comments to where the socket calls should be

--- README.md
+++ README.md
@@ -3,3 +3,48 @@ Reliable transport protocol assignment for CS 3251.  Phillip Bale &amp; Allen Ch
 
 
 DO NOT USE CYGWIN, THERE ARE KNOWN ISSUES WITH CYGWIN AND PYTHON 2.7's RAW_INPUT
+
+Testing things for now:
+
+
+FxA CLIENT:
+● Command-line: FxA-client X A P 
+
+The command-line arguments are: 
+
+X: the port number at which the FxA-client’s UDP socket should bind to (even number). Please remember that this port number should be equal to the server’s port number minus 1. 
+A: the IP address of NetEmu
+P: the UDP port number of NetEmu 
+
+*******************************************
+eg 'python FxA_client.py 3002 127.0.0.1 5000'
+*******************************************
+
+● Command: connect - The FxA-client connects to the FxA-server (running at the same IP host). 
+
+● Command: get F - The FxA-client downloads file F from the server (if F exists in the same directory with the FxA-server program). 
+
+● Command: post F - The FxA-client uploads file F to the server (if F exists in the same directory with the FxA-client program). This feature will be treated as extra credit for up to 20 project points.
+
+● Command: window W (only for projects that support configurable flow window) W: the maximum receiver’s window-size at the FxA-Client (in segments). 
+
+● Command: disconnect - The FxA-client terminates gracefully from the FxA-server. 
+
+
+FxA Server:
+● Command-line: FxA-server X A P 
+
+The command-line arguments are:
+X: the port number at which the FxA-server’s UDP socket should bind to (odd number) 
+A: the IP address of NetEmu
+P: the UDP port number of NetEmu 
+
+*******************************************
+eg 'python FxA_client.py 3003 127.0.0.1 5000'
+*******************************************
+
+● Command: window W (only for projects that support pipelined and bi- directional transfers) 
+
+W: the maximum receiver’s window-size at the FxA-Server (in segments). 
+
+● Command: terminate Shut-down FxA-Server gracefully. 
--- fxa/FxA_Client.py
+++ fxa/FxA_Client.py
@@ -5,132 +5,149 @@ import sys
 # for ip address validation
 from socket import inet_aton
 
+def main():
+    #uncomment
+    #clientSocket = RxP()
 
-# check for number of correct command line arguments
-if len(sys.argv) != 4:
-    print ('Please enter arguments in the correct format: X A P' + 
-        '\nX: The port number this client should bind to'+
-        '\nA: The IP address of NetEmu' + 
+    # check for number of correct command line arguments
+    if len(sys.argv) != 4:
+        print ('Please enter arguments in the correct format: X A P' + 
+            '\nX: The port number this client should bind to'+
+            '\nA: The IP address of NetEmu' + 
 
-        '\nP: The UDP port number of NetEmu ')
-    sys.exit()
+            '\nP: The UDP port number of NetEmu ')
+        sys.exit()
 
-# port number at which the FxA-client's UDP socket should bind to. Should be equal to the server's port number minus 1
-try:
-    portnumber = int(sys.argv[1])
-except:
-    print 'Please enter a valid port number for client (1025-65536)'
-    sys.exit()
+    # port number at which the FxA-client's UDP socket should bind to. Should be equal to the server's port number minus 1
+    try:
+        portnumber = int(sys.argv[1])
+    except:
+        print 'Please enter a valid port number for client (1025-65536)'
+        sys.exit()
 
-ipaddress = sys.argv[2]
+    ipaddress = sys.argv[2]
 
-try:
-    emuportnumber = int(sys.argv[3])
-except:
-    print 'Please enter a valid port number for NetEmu (1025-65536)'
-    sys.exit()
+    try:
+        emuportnumber = int(sys.argv[3])
+    except:
+        print 'Please enter a valid port number for NetEmu (1025-65536)'
+        sys.exit()
 
-file = ""
-size = 1
+    file = ""
+    # window size in segments
+    windowSize = 1
 
-# check for valid client port numbers
-if portnumber < 1025 or portnumber > 65536: 
-    print 'Please enter a valid port number for client (1025-65536)'
-    sys.exit()
+    # check for valid client port numbers
+    if portnumber < 1025 or portnumber > 65536: 
+        print 'Please enter a valid port number for client (1025-65536)'
+        sys.exit()
 
-# Port number for client must be even
-if portnumber % 2 != 0:
-    print 'Port number for client must be even'
-    sys.exit()
+    # Port number for client must be even
+    if portnumber % 2 != 0:
+        print 'Port number for client must be even'
+        sys.exit()
 
-# check for valid IP addresses
-try:
-    inet_aton(ipaddress)
-except:
-    print("Not a valid IP address")
-    sys.exit()
+    # check for valid IP addresses
+    try:
+        inet_aton(ipaddress)
+    except:
+        print("Not a valid IP address")
+        sys.exit()
 
-# check for valid NetEmu UDP port number
-if emuportnumber < 1025 or emuportnumber > 65536: 
-    print 'Please enter a valid port number for NetEmu (1025-65536)'
-    sys.exit()
+    # check for valid NetEmu UDP port number
+    if emuportnumber < 1025 or emuportnumber > 65536: 
+        print 'Please enter a valid port number for NetEmu (1025-65536)'
+        sys.exit()
+
+    # When everything passes, bind the RxP socket to passed in ip_address and port_number
+    #clientSocket.createRxPSocket(ipaddress, portnumber)
+
+    # Set window size
+    #clientSocket.setWindowSize(clientSocket, size)
+
+
+    print "Welcome! Commands include: \n'connect' \n'get <file>' \n'post <file>' \n'window <size>' \n'disconnect'"
+
+    #the command loop
+    while True:
+
+        # have the user input a command and parse it
+        command = raw_input('\nCommand: ').split(" ")
+        # check for invalid command
+        if len(command) > 2:
+            print "Enter a valid command"
+            continue
+
+        #check for connect and disconnect
+        if len(command) == 1:
+
+            if command[0] == "connect":
+                connect()
+                continue
+            elif command[0] == "disconnect":
+                disconnect()
+                continue
+            else:
+                print "Please enter a valid command"
+                continue
+        #check for get, post, and window
+        elif len(command) == 2:
+            #get
+            if command[0] == "get":
+                file = str(command[1])
+                get(file)
+                continue
+            #post
+            if command[0] == "post":
+                file = str(command[1])
+                post(file)
+                continue
+            #window
+            if command[0] == "window":
+                #int type checking
+                try:
+                    size = int(command[1])
+                except:
+                    print "invalid window size"
+                    continue
+                window(windowSize)
+                continue
+            else:
+                print "Please enter a valid command"
+                continue
+
 
 #Connect the client to the FxA-server
 def connect(): 
-    print "connecting to server"
+    print "Connecting to server"
     #TODO
+    #clientSocket.connectToRxP
     return
 
 # Download the file specified if it exists on the server
 def get(filename):
-    print "getting file: " + str(filename)
+    print "Downloading file: " + str(filename)
     #TODO
+    #clientSocket.recieveData
     return
 
 # Upload file to server
 def post(filename):
-    print "uploading file: " + str(filename)
+    print "Uploading file file: " + str(filename)
     #TODO
     return
 
 #updates client's maximum window-size
 def window(newsize):
-    print "updated window size to " + str(newsize)
-    size = newsize
+    print "Updated window size to " + str(newsize)
+    windowSize = newsize
+    #clientSocket.setWindowSize(windowSize)
 
 #disconnect from server
 def disconnect():
-    print "disconnecting"
+    print "Disconnecting"
     #TODO
     return
 
-print "Welcome! Commands include: \n'connect' \n'get <file>' \n'post <file>' \n'window <size>' \n'disconnect'"
 
-
-#the command loop
-while True:
-
-    # have the user input a command and parse it
-    command = raw_input('\nCommand: ').split(" ")
-    # check for invalid command
-    if len(command) > 2:
-        print "Enter a valid command"
-        continue
-
-    #check for connect and disconnect
-    if len(command) == 1:
-
-        if command[0] == "connect":
-            connect()
-            continue
-        elif command[0] == "disconnect":
-            disconnect()
-            continue
-        else:
-            print "Please enter a valid command"
-            continue
-    #check for get, post, and window
-    elif len(command) == 2:
-        #get
-        if command[0] == "get":
-            file = str(command[1])
-            get(file)
-            continue
-        #post
-        if command[0] == "post":
-            file = str(command[1])
-            post(file)
-            continue
-        #window
-        if command[0] == "window":
-            #int type checking
-            try:
-                size = int(command[1])
-            except:
-                print "invalid window size"
-                continue
-            window(size)
-            continue
-        else:
-            print "Please enter a valid command"
-            continue
+main()
